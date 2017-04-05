3e0be435bf6a9441614fd830cbbdd204c36e73b9
sergey <sergey@sergey-Lenovo-B590>
2017-04-05 20:54:01 +0300

    init lenovo

---
+++ src/ru/sbespalko/test/concurrency/TestThreads.java
@@ -0,0 +1,80 @@
+package ru.sbespalko.test.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  ласс <code>TestRunnable</code> демонстрирует работу потоков в много€дерной
+ * системе. ѕри задании числа потоков меньшим числа €дер(+hyperthreading - 1)
+ * быстродействие каждого потока не уменьшаетс€. 
+ * ѕри использовании hyperthreading 1 поток резервируетс€ дл€ диспетчеризации. 
+ * Ќа печать выводитс€ - врем€ работы с учетом запуска/остановки процессов. 
+ *  ол-во операций ++, выполненное всеми потоками за все врем€. 
+ *  ол-во операций ++, выполненное всеми потоками за 1 секунду. 
+ *  ол-во операций ++, выполненное 1 потоком за 1 секунду. 
+ * Ќа текстах (i7 4-core*2(hyper) 3.4Ghz)получаетс€ ~1/6 - 1/10 от частоты процессора.
+ * 
+ * @author Bespalko
+ *
+ */
+
+public class TestThreads {
+	private int testingTime;
+	private int threadsNum;
+	private volatile boolean stopRequested;
+	private volatile int summOps = 0;
+	List<Thread> list;
+
+	TestThreads(int testingTime, int threadsNum) {
+		this.testingTime = testingTime;
+		this.threadsNum = threadsNum;
+		list = new ArrayList<>();
+	}
+
+	public void printResults(long startTime) {
+		System.out.format("Working time: %5.2f sec %n", (System.nanoTime() - startTime) / 1e9);
+		System.out.format("SummOps in %2d sec with %2d threads(ideal %6d): %6d mln%n", testingTime, threadsNum,
+				13600 * testingTime, summOps);
+		System.out.format("SummOps in 1 sec with %2d threads(ideal 13600 mln) %5d mln%n", threadsNum,
+				summOps / testingTime);
+		System.out.format("SummOps in 1 sec with 1 thread (ideal 3400): %d mln%n",
+				summOps / (testingTime * threadsNum));
+	}
+
+	public void test() throws InterruptedException {
+
+		class backCounter extends Thread {
+
+			long i = 0;
+
+			@Override
+			public void run() {
+				while (!stopRequested) {
+					i++;
+				}
+				summOps += (int) (i / 1e6);
+			}
+
+		}
+
+		for (int th = 0; th < threadsNum; th++) {
+			list.add(new backCounter());
+		}
+		long startTime = System.nanoTime();
+		for (Thread thread : list) {
+			thread.start();
+		}
+		TimeUnit.SECONDS.sleep(testingTime);
+		stopRequested = true;
+		for (Thread thread : list) {
+			while (thread.isAlive()) {
+			}
+		}
+		this.printResults(startTime);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new TestThreads(10, 3).test();
+	}
+}
---
+++ src/ru/sbespalko/test/concurrency/WaitNotify.java
@@ -0,0 +1,44 @@
+package ru.sbespalko.test.concurrency;
+
+public class WaitNotify {
+
+	public static void main(String[] args) {
+		ThreadB b = new ThreadB();
+		b.main = Thread.currentThread();
+		b.start();
+
+		synchronized (b) {
+			try {
+				System.out.println("Waiting for b to complete...");
+				b.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Total is: " + b.total);
+		}
+		int summ = 0;
+		for (int i = 0; i < 100; i++) {
+			summ += i;
+		}
+		System.out.println(summ);
+
+	}
+}
+
+class ThreadB extends Thread {
+	int total;
+	static Thread main;
+
+	public void run() {
+		// сихронизируемся по this из-за того, что используем notify()
+		// работает и без синхронизации
+		synchronized (this) {
+			for (int i = 0; i < 100; i++) {
+				total += i;
+			}
+
+			notify();
+		}
+		
+	}
+}
