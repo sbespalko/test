e8d7fe72ed3ae6c26741e245bfef84fa90ec3dcf
sergey <sergey@sergey-Lenovo-B590>
2017-04-06 22:43:18 +0300

    runnable -> high lvl concurrency

--- src/ru/sbespalko/test/concurrency/NameList.java
+++ src/ru/sbespalko/test/concurrency/NameList.java
@@ -5,26 +5,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class NameList {
-	private List names = Collections.synchronizedList(new LinkedList());
+	private List<String> names = Collections.synchronizedList(new LinkedList<>());
 
 	public void add(String name) {
 		names.add(name);
 	}
 
 	public synchronized String removeFirst() {
-		if (names.size() > 0)
+		if (names.size() > 0) {
 			return (String) names.remove(0);
-		else
+		} else {
 			return null;
+		}
 	}
 
 	public static void main(String[] args) {
 		final NameList nl = new NameList();
 		nl.add("Ozymandias");
 		class NameDropper extends Thread {
+			@Override
 			public void run() {
-					String name = nl.removeFirst();
-					System.out.println(name);
+				String name = nl.removeFirst();
+				System.out.println(name);
 			}
 		}
 		Thread t1 = new NameDropper();
